00d387e02bd108d0a70d56f1f1f180aadcf502b2
ManiacMoustache <43834579+ManiacMoustache@users.noreply.github.com>
2019-11-20 20:14:06 -0700

    checkNumber
    checkNumber

--- GuessTheNumber/guessingGameLogic.py
+++ GuessTheNumber/guessingGameLogic.py
@@ -13,12 +13,23 @@ class GuessingGameLogic():
 
     def __init__(self, minSec = 1, maxSec = 20, numGuess = 3):
         #start the game
-        gameState = GameState.kGuessing
+        self.gameState = GameState.kGuessing
 
         #generate a random number
-        secretNumber = random.randint(minSec, maxSec)
+        self.secretNumber = random.randint(minSec, maxSec)
 
         #set the number of tries to guess the number
-        guessesLeft = numGuess
-    
-    def 
+        self.guessesLeft = numGuess
+    def getGameState(self):
+        return self.gameState
+    def checkNumber(self, guess):
+        if self.guessesLeft<=0:
+            self.gameState = GameState.kLost
+            return "Out of guesses!"
+        elif guess > self.secretNumber:
+            return "Too high!"
+        elif guess == self.secretNumber:
+            self.gameState = GameState.kWon
+            return "Guessed!"
+        elif guess < self.secretNumber:
+            return "Too low!"
--- GuessTheNumber/numberGameV1.py
+++ GuessTheNumber/numberGameV1.py
@@ -18,7 +18,7 @@ print("Only use whole numbers. 'Five' does not work.")
 
 
 #loop
-while gameState == ggl.GameState.kGuessing:
+while game.getGameState() == ggl.GameState.kGuessing:
     #get user guess
     try:
         userGuess = int(input("Guess the number:"))
@@ -26,21 +26,6 @@ while gameState == ggl.GameState.kGuessing:
         print("Please RTFM")
 
     #see if guess is correct?
-    if secretNumber == userGuess:
-        print("You are as awsome as Cooper but he still wins.")
-        gameState = "won"
-        continue
-    elif secretNumber < userGuess:
-        print("Too High")
-    else:
-        print("Too Low")
-    
-    guessesLeft -= 1
-    print(f"You have {guessesLeft} guesses left")
-
-    if not guessesLeft:
-        gameState = "lost"
-        print("You lost. There was actual number was %d"%(secretNumber))
-        continue
+    print(game.checkNumber(userGuess))
 
 
