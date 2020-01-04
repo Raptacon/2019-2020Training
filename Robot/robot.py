72b4a68a0e4061ee5081ff738604e0e4a252d2cc
ManiacMoustache <43834579+ManiacMoustache@users.noreply.github.com>
2020-01-04 08:58:54 -0700

    Has encoder and PIDcontroller initialization, has not been tested, distanceperpulse and output is set up in degrees.

--- Robot/robot.py
+++ Robot/robot.py
@@ -12,16 +12,27 @@ class MyRobot(wpilib.SampleRobot):
     def robotInit(self):
         self.controller = wpilib.Joystick(0)
         self.motor = ctre.WPI_TalonSRX(0)
-        self.pwMotor = wpilib.PWMSpeedController(0)
         log.info("robot initialized")
+        self.encoderInit(1, 0, 0, 1, 50, 1, 1, 1, 1, self.motor, 50, -360, 360, -1, 1) #Most numbers should be checked, especially PIDF. Distance per pulse should be determined in degrees (makes sense)
 
+    def encoderInit(self, A, B, distPerPulse, minRate, maxPeriod, P, I, D, F, motor, period, minInput, maxInput, minOutput, maxOutput):
+        self.encoder = wpilib.Encoder(A, B)
+        self.encoder.setReverseDirection(True)
+        self.encoder.setDistancePerPulse(distPerPulse)
+        self.encoder.setMinRate(minRate)
+        self.encoder.setMaxPeriod(maxPeriod)
+        self.PIDcontrol = wpilib.PIDController(P, I, D, F, self.encoder, motor, period)
+        self.PIDcontrol.setInputRange(minInput, maxInput)
+        self.PIDcontrol.setContinuous()
+        self.PIDcontrol.setOutputRange(minOutput, maxOutput)
+        self.PIDcontrol.enable()
+        
     def operatorControl(self):
         log.info("operator control")
         while self.isOperatorControl and self.isEnabled:
-            log.debug("joystick is %f y %f x", self.controller.getY(), self.controller.getX())
+            log.debug("joystick is %f y", self.controller.getY(), self.controller.getX())
             wpilib.Timer.delay(.1)
-            self.motor.set(self.controller.getY())
-            self.pwMotor.set(self.controller.getX())
+            self.PIDcontrol.setSetpoint(self.controller.getY()*360)
 
 if __name__ == "__main__":
     main()
